9928173883f965e3efb39ec8ed974fd967c23972
junicotandiago198 <junicotandiagoo21@gmail.com>
2023-03-26 20:47:21 +0700

    finsh project 1

---
+++ script.js
@@ -0,0 +1,28 @@
+async function fetchData() {
+  const response = await fetch('https://pokeapi.co/api/v2/pokemon/pikachu', {
+    method: 'GET',
+  });
+
+  const json = await response.json();
+  let id = json.id;
+  let name = json.name;
+  let image = json.sprites.front_default;
+  let types = json.types[0].type.name;
+
+  console.log(image);
+
+  // document.getElementById("pokemon").innerHTML =`<p>${id}</p>
+  // <p>${name}</p>
+  // <img src="${image}" >
+  // <p>${types}</p>
+  // `;
+
+  const divPoke = document.getElementById('pokemon');
+  let p1 = document.createElement('p');
+  p1.innerHTML = `${id} : ${name}`;
+  let img = document.createElement('img');
+  img.src = `${image}`;
+  let p2 = document.createElement('p');
+  p2.innerHTML = `type: ${types}`;
+  divPoke.append(p1, img, p2);
+}
